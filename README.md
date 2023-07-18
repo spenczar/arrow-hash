7de01be083c9c9549df5ca68afadcbe1ce41311e
Spencer Nelson <spencer@b612foundation.org>
2023-07-18 16:03:24 -0700

    measure in release mode, duh

--- README.md
+++ README.md
@@ -2,32 +2,9 @@
 
 A hacked-out 2-hour prototype to investigate hash tables over arrow arrays.
 
-Callled from Python, lookups are about 6us.  Table construction is about 250ns per array element (250ms for 1m elements)
+| operation              | python dict | arrow-hash (us |
+|------------------------|-------------|----------------|
+| index 1 million points | 550ms       | 40ms           |
+| lookup                 | 1us         | 1.5us          |
 
-In comparison, for a plain Python dict, lookups are about 1us, and table construction is about 600ns per element.
 
-```
-In [1]: import numpy as np; import pyarrow as pa; import arrow_hash
-
-In [2]: data = pa.array(np.random.choice(np.arange(1000), 100000))
-
-In [3]: %timeit arrow_hash.ArrowInt64Index(data)
-27.4 ms ± 147 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
-
-In [4]: data = pa.array(np.random.choice(np.arange(100), 100000))
-
-In [5]: %timeit arrow_hash.ArrowInt64Index(data)
-25.7 ms ± 73.6 µs per loop (mean ± std. dev. of 7 runs, 10 loops each)
-
-In [6]: data = pa.array(np.random.choice(np.arange(100), 100))
-
-In [7]: %timeit arrow_hash.ArrowInt64Index(data)
-38.9 µs ± 65.7 ns per loop (mean ± std. dev. of 7 runs, 10,000 loops each)
-
-In [8]: data = pa.array(np.random.choice(np.arange(100), 200))
-
-In [9]: %timeit arrow_hash.ArrowInt64Index(data)
-70.9 µs ± 127 ns per loop (mean ± std. dev. of 7 runs, 10,000 loops each)
-
-In [10]: 
-```
--- src/lib.rs
+++ src/lib.rs
@@ -8,42 +8,42 @@ use pyo3::prelude::*;
 
 #[pyclass]
 struct ArrowUInt8Index {
-    hashtable: hashbrown::HashMap<u8, Vec<u64>>,
+    hashtable: hashbrown::HashMap<u8, array::UInt64Array>,
 }
 
 #[pyclass]
 struct ArrowUInt16Index {
-    hashtable: hashbrown::HashMap<u16, Vec<u64>>,
+    hashtable: hashbrown::HashMap<u16, array::UInt64Array>,
 }
 
 #[pyclass]
 struct ArrowUInt32Index {
-    hashtable: hashbrown::HashMap<u32, Vec<u64>>,
+    hashtable: hashbrown::HashMap<u32, array::UInt64Array>,
 }
 
 #[pyclass]
 struct ArrowUInt64Index {
-    hashtable: hashbrown::HashMap<u64, Vec<u64>>,
+    hashtable: hashbrown::HashMap<u64, array::UInt64Array>,
 }
 
 #[pyclass]
 struct ArrowInt8Index {
-	hashtable: hashbrown::HashMap<i8, Vec<u64>>,
+	hashtable: hashbrown::HashMap<i8, array::UInt64Array>,
 }
 
 #[pyclass]
 struct ArrowInt16Index {
-	hashtable: hashbrown::HashMap<i16, Vec<u64>>,
+	hashtable: hashbrown::HashMap<i16, array::UInt64Array>,
 }
 
 #[pyclass]
 struct ArrowInt32Index {
-	hashtable: hashbrown::HashMap<i32, Vec<u64>>,
+	hashtable: hashbrown::HashMap<i32, array::UInt64Array>,
 }
 
 #[pyclass]
 struct ArrowInt64Index {
-	hashtable: hashbrown::HashMap<i64, Vec<u64>>,
+	hashtable: hashbrown::HashMap<i64, array::UInt64Array>,
 }
 
 // This code uses macros because pyclass doesn't support generics.
@@ -51,12 +51,17 @@ macro_rules! index_for_primitive {
     ($struct_name:ident, $arrow_type: ty, $rust_type: ty) => {
         impl From<array::PrimitiveArray<$arrow_type>> for $struct_name {
             fn from(data: array::PrimitiveArray<$arrow_type>) -> Self {
-                let mut hashtable = hashbrown::HashMap::with_capacity(data.len());
+                let mut builder_table = hashbrown::HashMap::with_capacity(data.len());
                 for i in 0..data.len() {
-                    let vec = hashtable.entry(data.value(i)).or_insert(Vec::with_capacity(10));
-                    vec.push(i as u64);
+                    let builder = builder_table.entry(data.value(i)).or_insert(array::UInt64Builder::new());
+		    builder.append_value(i as u64);
                 }
-                Self { hashtable }
+
+		let mut hashtable = hashbrown::HashMap::with_capacity(builder_table.len());
+		for (key, mut builder) in builder_table {
+		    hashtable.insert(key, builder.finish());
+		}
+		Self { hashtable }
             }
         }
 
@@ -64,7 +69,7 @@ macro_rules! index_for_primitive {
             fn get(
                 &self,
                 key: $rust_type,
-            ) -> Option<&Vec<u64>> {
+            ) -> Option<&array::UInt64Array> {
                 self.hashtable.get(&key)
             }
         }
@@ -87,7 +92,7 @@ macro_rules! test_primitive_index {
 	    let array = array::PrimitiveArray::<$arrow_type>::from(vec![1, 2, 3, 3, 5]);
 	    let idx = $struct_name::from(array);
 	    let have = idx.get(1 as $rust_type);
-	    let want = vec![0];
+	    let want = array::UInt64Array::from(vec![0]);
 	    assert_eq!(*have.unwrap(), want);
 	}
     };
@@ -108,14 +113,14 @@ fn test_build_primitive_index() {
 impl ArrowInt64Index {
     #[pyo3(name="get")]
     fn get_py(&self, key: i64, py: Python<'_>) -> PyResult<Option<PyObject>> {
-	let vec = self.get(key);
-	if vec.is_none() {
+	let array = self.get(key);
+	if array.is_none() {
 	    return Ok(None);
 	}
-	let vec = vec.unwrap();
-	let array: array::ArrayData = array::UInt64Array::from(vec.clone()).into();
+	let array = array.unwrap();
+	let array_data: array::ArrayData = array.clone().into();
 
-	let pyarray = array.to_pyarrow(py)?;
+	let pyarray = array_data.to_pyarrow(py)?;
 	Ok(Some(pyarray))
 
     }
@@ -130,37 +135,6 @@ impl ArrowInt64Index {
 	let idx = Self::from(array);
 	Ok(idx)
     }
-}
-
-#[pyclass]
-struct ArrowStringIndex {
-    hashtable: hashbrown::HashMap<String, Vec<u64>>,
-}
-
-impl From<array::StringArray> for ArrowStringIndex {
-    fn from(data: array::StringArray) -> Self {
-	let mut hashtable = hashbrown::HashMap::with_capacity(data.len() * 2);
-	for i in 0..data.len() {
-	    let vec = hashtable.entry(data.value(i).to_string()).or_insert(Vec::new());
-	    vec.push(i as u64);
-	}
-	Self { hashtable }
-    }
-}
-
-impl ArrowStringIndex {
-    fn get(&self, key: &str) -> Option<&Vec<u64>> {
-        self.hashtable.get(key)
-    }
-}
-
-#[test]
-fn test_build_string_index() {
-    let array = array::StringArray::from(vec!["a", "b", "c", "c", "e"]);
-    let idx = ArrowStringIndex::from(array);
-    let have = idx.get("a");
-    let want = vec![0];
-    assert_eq!(*have.unwrap(), want);
 }
 
 /// Formats the sum of two numbers as string.
